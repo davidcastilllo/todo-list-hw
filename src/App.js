144ddf173e560b7a3b41f65377573c7a158e4acd
David Castillo <seth.castillo4@gmail.com>
2022-04-11 21:35:02 -0500

    finished HW

--- src/App.js
+++ src/App.js
@@ -1,25 +1,64 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
 
-function App() {
+class App extends Component {
+ constructor(props) {
+  super(props)
+
+  this.state = {
+   isClicked: false,
+   todos: [
+    'eat',
+    'sleep',
+    'code'
+   ],
+   text: ''
+  }
+ }
+
+componentDidUpdate() {
+ console.log(this.state.todos)
+}
+
+ handleClick = (event) => {
+  this.setState({
+   todos: [...this.state.todos, this.state.text],
+   text: ''
+  })
+
+ }
+
+ handleChange = (event) => {
+  this.setState({
+   text: event.target.value
+  })
+ }
+
+ removeItem = (e) => {
+  let i = this.state.todos.indexOf(e)
+  let UpdatedTodo = [...this.state.todos]
+  UpdatedTodo.splice(i,1)
+  this.setState({
+   todos: [...UpdatedTodo]
+  })
+ }
+
+ render() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <button onClick={this.handleClick}>Click me</button>
+    <input type="text" onChange={this.handleChange} value={this.state.text} />
+    <ul>
+     {this.state.todos.map((items) => {
+      return <li>
+       {items}
+       <button onClick={((e) => {this.removeItem(items)})}>Click to remove item</button>
+       </li>
+     })}
+    </ul>
     </div>
   );
+ }
+  
 }
 
 export default App;
